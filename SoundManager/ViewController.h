75197b1fbfb2e29b1762bf1687dc7a0403810815
Micha Faw <michafaw@mf-macbook-pro.local>
2016-01-19 22:00:06 -0500

    Initial Commit

---
+++ SoundManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SoundManager
+//
+//  Created by Micha Faw on 1/19/16.
+//  Copyright © 2016 Micha Faw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SoundManager/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SoundManager
+//
+//  Created by Micha Faw on 1/19/16.
+//  Copyright © 2016 Micha Faw. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
